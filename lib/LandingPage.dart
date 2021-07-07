9ba9c88099f88f14022cb78d4c785e5b83774d0c
brijesh <brijeshchauhan7895@gmail.com>
2021-07-07 16:36:30 +0530

    add google ads

--- lib/LandingPage.dart
+++ lib/LandingPage.dart
@@ -4,58 +4,110 @@ import 'package:flutter_app_bloc/BLoC.dart';
 import 'package:flutter_app_bloc/Event.dart';
 import 'package:flutter_app_bloc/model/Pokemon.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class LandingPage extends StatefulWidget {
   @override
   _LandingPageState createState() => _LandingPageState();
 }
 
-class _LandingPageState extends State<LandingPage> {
+class _LandingPageState extends State<LandingPage> with WidgetsBindingObserver {
   final _bloc = BLoC();
+  BannerAd myBanner;
+  InterstitialAd myInterstitial;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    initialzeBanners();
+    initalizeInterstial();
+    myBanner.load();
     _bloc.counter_event_sink.add(ApiEvent());
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      // initialzeBanners();
+      // initalizeInterstial();
+      // myBanner.load();
+      // _bloc.counter_event_sink.add(ApiEvent());
+    }
+  }
+
+  void initalizeInterstial() {
+
+    InterstitialAd.load(
+        adUnitId: 'ca-app-pub-3940256099942544/8691691433',
+        request: AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (InterstitialAd ad) {
+            // Keep a reference to the ad so you can show it later.
+            myInterstitial = ad;
+            myInterstitial.show();
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('InterstitialAd failed to load: $error');
+          },
+        ));
+
+  }
+
+  void initialzeBanners() {
+    AdSize adSize = AdSize(width: 300, height: 60);
+    myBanner = BannerAd(
+      adUnitId: 'ca-app-pub-7410464693885383/1095703406',
+      size: adSize,
+      request: AdRequest(),
+      listener: BannerAdListener(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Block Patter Demo"),automaticallyImplyLeading: false,),
+        appBar: AppBar(
+          title: Text("Block Patter Demo"), automaticallyImplyLeading: false,),
         body: buildPokemonContent());
   }
 
   Widget buildPokemonContent() {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          StreamBuilder(
-            stream: _bloc.stream_counter,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return buildCircularProgressIndicatorWidget();
-              }
-              else if (snapshot.hasError) {
-                showSnackBar(context, snapshot.error.toString());
-                return buildListViewNoDataWidget();
-              }
-              else if (snapshot.connectionState == ConnectionState.active) {
-                var pokemonList = snapshot.data;
-                if (null != pokemonList)
-                  return buildListViewWidget(pokemonList);
-                else
+    return Stack(
+      children: <Widget>[Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            StreamBuilder(
+              stream: _bloc.stream_counter,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return buildCircularProgressIndicatorWidget();
+                }
+                else if (snapshot.hasError) {
+                  showSnackBar(context, snapshot.error.toString());
                   return buildListViewNoDataWidget();
-              }
-              else
-                return buildListViewNoDataWidget();
-
-            },
-          )
-        ],
-      ),
+                }
+                else if (snapshot.connectionState == ConnectionState.active) {
+                  var pokemonList = snapshot.data;
+                  if (null != pokemonList)
+                    return buildListViewWidget(pokemonList);
+                  else
+                    return buildListViewNoDataWidget();
+                }
+                else
+                  return buildCircularProgressIndicatorWidget();
+              },
+            )
+          ],
+        ),
+      ), Align(alignment: Alignment.bottomCenter,
+          child: Container(
+            child: AdWidget(ad: myBanner,),
+            width: myBanner.size.width.toDouble(),
+            height: myBanner.size.height.toDouble(),
+          ))
+      ],
     );
   }
 
@@ -68,37 +120,41 @@ class _LandingPageState extends State<LandingPage> {
   Widget buildListViewWidget(List<Pokemon> pokemonList) {
     return Flexible(
         child: new ListView.builder(
-      padding: EdgeInsets.all(10),
-      itemCount: pokemonList.length,
-      itemBuilder: (BuildContext context, int index) {
-        var item = pokemonList[index];
-        var _colors = Colors.primaries;
-        final MaterialColor color = _colors[index % _colors.length];
-        return new ListTile(
-          dense: false,
-          leading: new CircleAvatar(
-            backgroundColor: Colors.white,
-            child: CachedNetworkImage(
-              imageUrl: item.url,
-              progressIndicatorBuilder: (context, url, downloadProgress) =>
-                  CircularProgressIndicator(value: downloadProgress.progress),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-            ),
-          ),
-          title: new Text(item.name),
-          subtitle: new Text(
-            "Weight: ${item.weight} Height: ${item.height} ",
-            style: Theme.of(context).textTheme.caption,
-          ),
-          onTap: () {
-            // Navigator.push(
-            //     context,
-            //     new MaterialPageRoute(
-            //         builder: (context) => new ViewPokemonDetail()));
+          padding: EdgeInsets.all(10),
+          itemCount: pokemonList.length,
+          itemBuilder: (BuildContext context, int index) {
+            var item = pokemonList[index];
+            var _colors = Colors.primaries;
+            final MaterialColor color = _colors[index % _colors.length];
+            return new ListTile(
+              dense: false,
+              leading: new CircleAvatar(
+                backgroundColor: Colors.white,
+                child: CachedNetworkImage(
+                  imageUrl: item.url,
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      CircularProgressIndicator(
+                          value: downloadProgress.progress),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+              ),
+              title: new Text(item.name),
+              subtitle: new Text(
+                "Weight: ${item.weight} Height: ${item.height} ",
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .caption,
+              ),
+              onTap: () {
+                // Navigator.push(
+                //     context,
+                //     new MaterialPageRoute(
+                //         builder: (context) => new ViewPokemonDetail()));
+              },
+            );
           },
-        );
-      },
-    ));
+        ));
   }
 
   Widget buildListViewNoDataWidget() {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_bloc/LandingPage.dart';
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
